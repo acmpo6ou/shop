a931e29ed8bb8590ef83bd5bae52eab1f9b6cfad
acmpo6ou <acmpo6ou@gmail.com>
2023-03-10 10:29:20 +0100

    🪑 Implement ProductItem

--- app/src/main/java/com/acmpo6ou/shop/MainActivity.kt
+++ app/src/main/java/com/acmpo6ou/shop/MainActivity.kt
@@ -3,13 +3,24 @@ package com.acmpo6ou.shop
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.acmpo6ou.shop.ui.theme.ShopTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    Greeting("Android")
+                    ProductItemPreview()
                 }
             }
         }
@@ -30,14 +41,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ProductItem(name: String, info: String, price: String, imageUrl: String) {
+    Card(
+        modifier = Modifier
+            .border(BorderStroke(1.dp, Color.Black))
+            .padding(8.dp)
+            .wrapContentHeight(Alignment.Top)
+            .clickable {}
+            .fillMaxWidth(),
+        elevation = 10.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "$name image",
+                modifier = Modifier.size(100.dp),
+            )
+            Column {
+                Row {
+                    Text(
+                        text = name,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painterResource(id = R.drawable.add_shopping_cart),
+                            "Add $name to cart",
+                        )
+                    }
+                }
+                Text(text = info)
+                Text(
+                    text = price,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ProductItemPreview() {
     ShopTheme {
-        Greeting("Android")
+        ProductItem(
+            "Test name",
+            "Color: red\nMaterial: wood",
+            "300 kr",
+            "https://shop.static.ingka.ikea.com/PIAimages/0462849_PE608354_S4.JPG",
+        )
     }
 }
