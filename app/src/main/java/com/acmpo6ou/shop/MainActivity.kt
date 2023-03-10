236999012ffb4836a9b2c4c2824e8cc11ae0f20c
acmpo6ou <acmpo6ou@gmail.com>
2023-03-10 12:25:03 +0100

    ➡️  Move ProductItem to ProductList module

--- app/src/main/java/com/acmpo6ou/shop/MainActivity.kt
+++ app/src/main/java/com/acmpo6ou/shop/MainActivity.kt
@@ -4,26 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.acmpo6ou.shop.model.Product
 import com.acmpo6ou.shop.model.ProductRepo
+import com.acmpo6ou.shop.ui.ProductItemPreview
 import com.acmpo6ou.shop.ui.ProductsViewModel
 import com.acmpo6ou.shop.ui.theme.ShopTheme
 
@@ -46,74 +32,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun ProductItem(product: Product) {
-    Card(
-        modifier = Modifier
-            .border(BorderStroke(1.dp, Color.Black))
-            .padding(8.dp)
-            .wrapContentHeight(Alignment.Top)
-            .clickable {}
-            .fillMaxWidth(),
-        elevation = 10.dp,
-    ) {
-        Row(
-            modifier = Modifier.padding(8.dp),
-        ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(product.imageUrl)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = "${product.name} image",
-                modifier = Modifier.size(100.dp),
-            )
-            Column {
-                Row {
-                    Text(
-                        text = product.name,
-                        fontWeight = FontWeight.Bold,
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            painterResource(id = R.drawable.add_shopping_cart),
-                            "Add ${product.name} to cart",
-                        )
-                    }
-                }
-                Text(text = product.prettyInfo)
-                Text(
-                    text = product.prettyPrice,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 30.sp,
-                    textAlign = TextAlign.End,
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ProductItemPreview() {
-    ShopTheme {
-        ProductItem(
-            Product(
-                id = 1,
-                name = "Henriksdal",
-                price = Product.Price(499f, "kr"),
-                info = mapOf(
-                    "material" to "wood with cover",
-                    "color" to "white",
-                ),
-                type = "chair",
-                imageUrl = "https://shop.static.ingka.ikea.com/PIAimages/0462849_PE608354_S4.JPG",
-            ),
-        )
     }
 }
---
+++ app/src/main/java/com/acmpo6ou/shop/ui/ProductList.kt
@@ -0,0 +1,95 @@
+package com.acmpo6ou.shop.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.acmpo6ou.shop.R
+import com.acmpo6ou.shop.model.Product
+import com.acmpo6ou.shop.ui.theme.ShopTheme
+
+@Composable
+fun ProductItem(product: Product) {
+    Card(
+        modifier = Modifier
+            .border(BorderStroke(1.dp, Color.Black))
+            .padding(8.dp)
+            .wrapContentHeight(Alignment.Top)
+            .clickable {}
+            .fillMaxWidth(),
+        elevation = 10.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(product.imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "${product.name} image",
+                modifier = Modifier.size(100.dp),
+            )
+            Column {
+                Row {
+                    Text(
+                        text = product.name,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painterResource(id = R.drawable.add_shopping_cart),
+                            "Add ${product.name} to cart",
+                        )
+                    }
+                }
+                Text(text = product.prettyInfo)
+                Text(
+                    text = product.prettyPrice,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductItemPreview() {
+    ShopTheme {
+        ProductItem(
+            Product(
+                id = 1,
+                name = "Henriksdal",
+                price = Product.Price(499f, "kr"),
+                info = mapOf(
+                    "material" to "wood with cover",
+                    "color" to "white",
+                ),
+                type = "chair",
+                imageUrl = "https://shop.static.ingka.ikea.com/PIAimages/0462849_PE608354_S4.JPG",
+            ),
+        )
+    }
+}
