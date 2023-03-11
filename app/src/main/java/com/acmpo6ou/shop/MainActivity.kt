8902f8e6da9835a938687e40541d980958fa5f2f
acmpo6ou <acmpo6ou@gmail.com>
2023-03-11 11:17:43 +0100

    🗺️ Implement bottom nav bar

--- app/src/main/java/com/acmpo6ou/shop/MainActivity.kt
+++ app/src/main/java/com/acmpo6ou/shop/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.acmpo6ou.shop.model.ProductRepo
 import com.acmpo6ou.shop.model.ProductRepo.Companion.CART_IDS
@@ -30,21 +40,76 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ShopTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background,
-                ) {
-                    Column {
-                        NavHost(
-                            navController = rememberNavController(),
-                            startDestination = "product_list",
-                        ) {
-                            composable("product_list") { ProductListScreen(productsViewModel) }
-                            composable("cart") { CartScreen() }
-                        }
-                    }
-                }
+                MainContent(productsViewModel)
             }
+        }
+    }
+}
+
+enum class Screen(
+    val route: String,
+    @StringRes val labelId: Int,
+    @DrawableRes val iconId: Int,
+) {
+    PRODUCT_LIST(
+        "product_list",
+        R.string.product_list_label,
+        R.drawable.search,
+    ),
+    CART(
+        "cart",
+        R.string.cart_label,
+        R.drawable.shopping_cart,
+    ),
+}
+
+@Composable
+fun MainContent(productsViewModel: ProductsViewModel) {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomBar(navController) },
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = Screen.PRODUCT_LIST.route,
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            composable(Screen.PRODUCT_LIST.route) {
+                ProductListScreen(productsViewModel)
+            }
+            composable(Screen.CART.route) {
+                CartScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        Screen.values().forEach { screen ->
+            val label = stringResource(screen.labelId)
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painterResource(screen.iconId),
+                        contentDescription = label,
+                    )
+                },
+                label = { Text(label) },
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
         }
     }
 }
