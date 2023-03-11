43c91a24a4ddf4f170e204a9cebfe6463e303e24
acmpo6ou <acmpo6ou@gmail.com>
2023-03-11 13:14:48 +0100

    🔤 Implement empty cart placeholder

--- app/src/main/java/com/acmpo6ou/shop/ui/screen/cart/CartScreen.kt
+++ app/src/main/java/com/acmpo6ou/shop/ui/screen/cart/CartScreen.kt
@@ -1,9 +1,29 @@
 package com.acmpo6ou.shop.ui.screen.cart
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.acmpo6ou.shop.R
 import com.acmpo6ou.shop.ui.screen.ProductList
 
 @Composable
 fun CartScreen(viewModel: CartViewModel) {
-    ProductList(viewModel)
+    if (viewModel.products.size > 0) {
+        ProductList(viewModel)
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(stringResource(R.string.no_items))
+        }
+    }
 }
