b5ac3da35aee97eeba876f61cfed3808f1654a22
acmpo6ou <acmpo6ou@gmail.com>
2023-03-12 11:34:52 +0100

    ➡️  Move ToggleCart to ProductListScreen

--- app/src/main/java/com/acmpo6ou/shop/ui/screen/ProductList.kt
+++ app/src/main/java/com/acmpo6ou/shop/ui/screen/ProductList.kt
@@ -4,22 +4,20 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.acmpo6ou.shop.R
 import com.acmpo6ou.shop.model.Product
-import com.acmpo6ou.shop.ui.screen.productlist.ProductListViewModel
 
 @Composable
 fun ProductList(
@@ -77,22 +75,5 @@ fun ProductItem(
                 )
             }
         }
-    }
-}
-
-@Composable
-fun ToggleCart(viewModel: ProductListViewModel, product: Product) {
-    IconButton(onClick = { viewModel.onIconClicked(product) }) {
-        val cartIds = remember { viewModel.cartIds }
-        Icon(
-            painter = painterResource(id = R.drawable.shopping_cart),
-            tint = if (product.id in cartIds) {
-                MaterialTheme.colors.primary
-            } else {
-                MaterialTheme.colors.onBackground
-            },
-            // TODO: should be dynamic depending on add/remove
-            contentDescription = "Add ${product.name} to cart",
-        )
     }
 }
--- app/src/main/java/com/acmpo6ou/shop/ui/screen/productlist/ProductListScreen.kt
+++ app/src/main/java/com/acmpo6ou/shop/ui/screen/productlist/ProductListScreen.kt
@@ -1,13 +1,35 @@
 package com.acmpo6ou.shop.ui.screen.productlist
 
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+import com.acmpo6ou.shop.R
 import com.acmpo6ou.shop.model.Product
 import com.acmpo6ou.shop.ui.screen.ProductList
-import com.acmpo6ou.shop.ui.screen.ToggleCart
 
 @Composable
 fun ProductListScreen(viewModel: ProductListViewModel) {
     ProductList(viewModel) { product: Product ->
         ToggleCart(viewModel, product)
+    }
+}
+
+@Composable
+fun ToggleCart(viewModel: ProductListViewModel, product: Product) {
+    IconButton(onClick = { viewModel.onIconClicked(product) }) {
+        val cartIds = remember { viewModel.cartIds }
+        Icon(
+            painter = painterResource(id = R.drawable.shopping_cart),
+            tint = if (product.id in cartIds) {
+                MaterialTheme.colors.primary
+            } else {
+                MaterialTheme.colors.onBackground
+            },
+            // TODO: should be dynamic depending on add/remove
+            contentDescription = "Add ${product.name} to cart",
+        )
     }
 }
